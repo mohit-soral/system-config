d03f2b52dbf112f7ed0e0472444f7120d6e88268
Sachin Taksande <sachin.taksande@siemens.com>
2017-11-14 15:25:33 +0530

    feat: iptables

--- index.js
+++ index.js
@@ -4,58 +4,55 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var sysinfo = require('systeminformation');
 var fs = require('fs-extra');
-var tesserariusLib = require("tesserarius");
-var tesserarius = new tesserariusLib();
+var iptables = require('iptables');
+/*var tesserariusLib = require("tesserarius");
+var tesserarius = new tesserariusLib();*/
 
 var port = 9999;
 var app = express();
 
-
 app.use(bodyParser.json());
 
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
+app.get('/containers', (request, response) => {
+    console.log('In Method to get docker container info');
 
-app.get("/containers", (request, response) => {
-	console.log('In Method to get docker container info');
-
-	sysinfo.dockerAll((data) => {
-		console.log('Found details for ' + data.length + ' containers');
-		for(var i = 0 ; i < data.length ; i++) {
-			console.log(data[i].id + ' : ' + data[i].name + ' : ' + data[i].created + ' : ' + data[i].state + ' : ' + data[i].mem_percent + ' : ' + data[i].cpu_percent);
-		}
-	})
+    sysinfo.dockerAll((data) => {
+        console.log('Found details for ' + data.length + ' containers');
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].id + ' : ' + data[i].name + ' : ' + data[i].created + ' : ' + data[i].state + ' : ' + data[i].mem_percent + ' : ' + data[i].cpu_percent);
+        }
+    })
 
 })
 
-app.get('/proxy', function(req, res){
-    var result  = {
-        'http_proxy' : process.env.http_proxy || '',
-        'https_proxy' : process.env.https_proxy || ''
+app.get('/proxy', function (req, res) {
+    var result = {
+        'http_proxy': process.env.http_proxy || '',
+        'https_proxy': process.env.https_proxy || ''
     };
     console.log("result: " + JSON.stringify(result));
     res.send(result);
 });
 
-app.post('/proxy', function(req, res){
-    var proxyValues  = req.body;
+app.post('/proxy', function (req, res) {
+    var proxyValues = req.body;
     console.log("proxyValues: " + JSON.stringify(proxyValues));
 
     var valueToWrite = '';
-    if(proxyValues.http_proxy !== undefined || proxyValues.http_proxy !== ''){
+    if (proxyValues.http_proxy !== undefined || proxyValues.http_proxy !== '') {
         valueToWrite = valueToWrite + 'export http_proxy=' + proxyValues.http_proxy;
     }
-    if(proxyValues.https_proxy !== undefined || proxyValues.https_proxy !== ''){
+    if (proxyValues.https_proxy !== undefined || proxyValues.https_proxy !== '') {
         valueToWrite = valueToWrite + '\nexport https_proxy=' + proxyValues.https_proxy;
     }
-    
+
     const file = '/etc/profile.d/proxy.sh';
     fs.outputFileSync(file, valueToWrite)
     res.send();
 });
 
 
-const rule = {
+/*const rule = {
     interface: 'eth0',
     policy: 'ACCEPT',
     protocol: 'tcp',
@@ -63,38 +60,107 @@ const rule = {
     source: '10.0.10.0/24'
 }
 
-app.post('/createRule', function(req, res){
-	var ruleToCreate  = req.body;
-	tesserarius.add_rule('FirewallRuleChain', rule, (err) => {
-    if(err) {
-        throw err;
-    }
-	});
+app.post('/createRule', function (req, res) {
+    var rule = req.body;
+    tesserarius.add_rule('FirewallRuleChain', rule, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    console.log(rule);
+    res.send();
 });
 
-app.delete('/deleteRule', function(req, res){
-	var ruleToCreate  = req.body;
-	tesserarius.delete_rule('FirewallRuleChain', rule, (err) => {
-    if(err) {
-        throw err;
-    }
-	});
+app.get('/getRules', function (req, res) {
+    var rules = tesserarius.get_rules('FirewallRuleChain', (err, rules) => {
+        if (err) {
+            console.log(err);
+            return [];
+        }
+        return rules;
+    });
+    console.log(rules);
+    res.send(rules);
 });
 
-tesserarius.create_chain('FirewallRuleChain', (err) => {
-    if(err) {
-        throw err;
-    }
+app.delete('/deleteRule', function (req, res) {
+    var rule = req.body;
+    tesserarius.delete_rule('FirewallRuleChain', rule, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    console.log(rule);
+    res.send();
 });
 
 tesserarius.set_policy('FirewallRuleChain', 'DROP', (err) => {
-    if(err) {
-        throw err;
+    if (err) {
+        //console.log(err);
     }
 });
 
+tesserarius.create_chain('FirewallRuleChain', (err) => {
+    if (err) {
+        //console.log(err);
+    }
+});*/
 
+const ipTablesRuleFormat = {
+    in: 'eth0',
+    target: 'ACCEPT',
+    protocol: 'tcp',
+    dport: 22,
+    sport: 55,
+    src: '10.0.10.0/24',
+    dst: '0.0.0.0/0'
+}
+
+app.post('/allowRule', function (req, res) {
+    var rule = req.body;
+    rule.chain = 'FirewallRuleChain';
+    if(rule.src === undefined){
+        throw new Error('src not defined');
+    }
+    iptables.allow(rule);
+    console.log(rule);
+    res.send();
+});
+
+app.post('/rejectRule', function (req, res) {
+    var rule = req.body;
+    rule.chain = 'FirewallRuleChain';
+    if(rule.src === undefined){
+        throw new Error('src not defined');
+    }
+    iptables.reject(rule);
+    console.log(rule);
+    res.send();
+});
+
+app.post('/dropRule', function (req, res) {
+    var rule = req.body;
+    rule.chain = 'FirewallRuleChain';
+    iptables.drop(rule);
+    console.log(rule);
+    res.send();
+});
+
+app.get('/getRules', function (req, res) {
+    var rules = iptables.list('FirewallRuleChain', (rules) => {
+        console.log(rules);
+        res.send(rules);
+    });
+});
+
+app.delete('/deleteRule', function (req, res) {
+    var rule = req.body;
+    rule.chain = 'FirewallRuleChain';
+    iptables.deleteRule(rule);
+    console.log(rule);
+    res.send();
+});
 
 module.exports = app.listen(port, () => {
-	console.log('Server has started. Listening on port ' + port);
+    console.log('Server has started. Listening on port ' + port);
 })	
